2bde575cb7d0af82f890bea3adb7f51477a3ea21
Josh Burns <joshyburnss@gmail.com>
2024-05-28 16:48:36 +1000

    transparent background

--- lisp/ui.el
+++ lisp/ui.el
@@ -8,6 +8,9 @@
 (menu-bar-mode -1)  ; Leave this one on if you're a beginner!
 (tool-bar-mode -1)
 
+;; Make frames transparency
+(set-frame-parameter nil 'alpha-background 75)
+
 ;; Display line numbers in every buffer
 (global-display-line-numbers-mode 1)
 
