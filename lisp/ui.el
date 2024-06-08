d80ab93c7cf90dc42868fdbb0c140eb6e6b82db5
Josh Burns <joshyburnss@gmail.com>
2024-06-08 14:12:13 +1000

    turn on menu bar

--- lisp/ui.el
+++ lisp/ui.el
@@ -5,11 +5,8 @@
 ;;; Code:
 
 ;; Turn off some unneeded UI elements
-(menu-bar-mode -1)  ; Leave this one on if you're a beginner!
+;;(menu-bar-mode -1)  ; Leave this one on if you're a beginner!
 (tool-bar-mode -1)
-
-;; Make frames transparency
-(set-frame-parameter nil 'alpha-background 95)
 
 ;; Display line numbers in every buffer
 (global-display-line-numbers-mode 1)
