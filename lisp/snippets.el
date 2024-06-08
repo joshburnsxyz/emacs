007e6ecc234ef457c3cb08e20df6cb6513648394
Josh Burns <joshyburnss@gmail.com>
2024-06-08 13:36:31 +1000

    install emmet-mode

--- lisp/snippets.el
+++ lisp/snippets.el
@@ -11,8 +11,12 @@
 	'("~/.emacs.d/snippets"))
   (yas-global-mode 1))
 
-(use-package yasnippet-snippets
-  :ensure t)
+(use-package emmet-mode
+  :ensure t
+  :init
+  (add-hook 'sgml-mode-hook 'emmet-mode)
+  (add-hook 'css-mode-hook 'emmet-mode)
+  (setq emmet-move-cursor-between-quotes t))
 
 (provide 'snippets)
 
