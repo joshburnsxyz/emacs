b9eebe92cce5b3b3898aaa859ce8482d6023e719
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2025-04-24 11:48:47 +1000

    Update snippets.el

--- lisp/snippets.el
+++ lisp/snippets.el
@@ -4,17 +4,12 @@
 
 ;;; Code:
 
+(use-package yasnippet-snippets :ensure t)
 (use-package yasnippet
   :ensure t
   :init
-  (setq yas-snippet-dirs
-	'("~/.emacs.d/snippets"))
+  (yas-reload-all)
   (yas-global-mode 1))
-
-(use-package yasnippet-snippets
-  :ensure t
-  :config
-  (yas-reload-all))
 
 (use-package emmet-mode
   :ensure t
@@ -22,13 +17,6 @@
   (add-hook 'sgml-mode-hook 'emmet-mode)
   (add-hook 'css-mode-hook 'emmet-mode)
   (setq emmet-move-cursor-between-quotes t))
-
-;; Download yasnippet-snippets if ~/.emacs.d/snippets does not exists
-(unless (file-directory-p "~/.emacs.d/snippets")
-  (shell-command "git clone https://github.com/AndreaCrotti/yasnippet-snippets ~/.emacs.d/snippets-tmp")
-  (rename-file "~/.emacs.d/snippets-tmp/snippets" "~/.emacs.d/snippets")
-  (delete-directory "~/.emacs.d/snippets-tmp" 'recursive)
-  (message "Fetched snippets from GitHub"))
 
 (provide 'snippets)
 
